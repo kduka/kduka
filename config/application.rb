ca882cd5fff7690615d403de6b80aa6bbc15e20b
Martin Ndeto <martindeto@gmail.com>
2017-07-05 14:51:01 +0300

    precompile false

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module ShoppingCartExample
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.initialize_on_precompile = false
     
   end
 end
