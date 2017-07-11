db85116015c2d0e292ec93268f003590536d1d5c
Martin Ndeto <martindeto@gmail.com>
2017-07-11 22:41:22 +0300

    i dont

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
   @store  = Store.where(subdomain:@subdomain).first
 
     if @store.nil?
-      redirect_to("kduka."+request.domain)
+      redirect_to(request.domain)
     else
       @products = Product.where(store_id:@store.id)
       @order_item = current_order.order_items.new
