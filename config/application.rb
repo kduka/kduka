d52deb6098e73fa0381e759ffbd0ce6863637a8b
Martin Ndeto <martindeto@gmail.com>
2017-07-15 00:46:15 +0300

    1st?

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module ShoppingCartExample
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.initialize_on_precompile = false
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
     
     
     
