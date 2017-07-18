bda9a62e1ebd8e56978605fa7157b7441f78ae28
Martin Ndeto <martindeto@gmail.com>
2017-07-18 20:53:18 +0300

    store name

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -124,6 +124,6 @@ class StoresController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def store_params
-    params.require(:store).permit(:facebook,:linkedin,:twitter,:instagram,:pinterest,:vimeo,:youtube,:slogan)
+    params.require(:store).permit(:facebook,:linkedin,:twitter,:instagram,:pinterest,:vimeo,:youtube,:slogan, :subdomain)
   end
 end
