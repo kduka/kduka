ac228942841fba3a4434270c1335a03ed1d919bc
Martin Ndeto <martindeto@gmail.com>
2017-09-21 23:00:59 +0300

    Fixing category products display in Store to work with layouts

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -6,66 +6,6 @@ class CartsController < ApplicationController
   end
 
   def shipping
-    #require 'rubygems' if RUBY_VERSION < '1.9'
-    #require 'rest-client'
-
-    values = '{
-  "command": "request",
-  "data": {
-    "api_key": "mysendykey",
-    "api_username": "mysendyusername",
-    "from": {
-      "from_name": "Green House",
-      "from_lat": -1.300577,
-      "from_long": 36.78183,
-      "from_description": ""
-    },
-    "to": {
-      "to_name": "KICC",
-      "lat":"-1.183333",
-      "lng":"36.9166669",
-      "to_description": ""
-    },
-    "recepient": {
-      "recepient_name": "Sendy User",
-      "recepient_phone": "0728561783",
-      "recepient_email": "support@sendy.co.ke"
-    },
-    "delivery_details": {
-      "pick_up_date": "2016-04-20 12:12:12",
-      "collect_payment": {
-        "status": false,
-        "pay_method": 0,
-        "amount": 10
-      },
-      "return": true,
-      "note": " Sample note",
-      "note_status": true,
-      "request_type": "delivery",
-      "order_type": "ondemand_delivery",
-      "ecommerce_order": "ecommerce_order_001",
-      "skew": 1,
-      "package_size": [
-        {
-          "weight": 20,
-          "height": 10,
-          "width": 200,
-          "length": 30,
-          "item_name": "laptop"
-        }
-      ]
-    }
-  }
-}'
-
-    headers = {
-        :content_type => 'application/json'
-    }
-
-    response = RestClient.post 'https://apitest.sendyit.com/v1/#request', values, headers
-
-    @response = JSON.parse(response)
-
   set_shop
   end
 
