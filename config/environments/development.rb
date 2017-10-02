1c1bf1a172d840d4cec3167dd363198b6f566468
Martin Ndeto <martindeto@gmail.com>
2017-10-02 15:36:38 +0300

    Add Mail to Contact Form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {:address => 'localhost', :port => 1025}
+ config.action_mailer.smtp_settings = {:address => 'localhost', :port => 1025}
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -37,4 +37,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
+
 end
