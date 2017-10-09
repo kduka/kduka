adcb15dcbcfe2edb9fa16d2a0a60b06565c99f44
Martin Ndeto <martindeto@gmail.com>
2017-10-09 19:49:04 +0300

    Checking Domains and SUbdomains

--- config/routes.rb
+++ config/routes.rb
@@ -60,6 +60,7 @@ Rails.application.routes.draw do
   resources :store_campaigns
   resources :coupons
   resources :layouts
+  resources :home
 
   root to: "home#index"
   get '*path' => redirect('/')
