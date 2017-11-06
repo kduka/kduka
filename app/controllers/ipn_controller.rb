eb6aa62e4f4db368bca995c72b82ae48a31f09ae
Martin Ndeto <martindeto@gmail.com>
2017-11-06 21:59:45 +0300

    ipn change

--- app/controllers/ipn_controller.rb
+++ app/controllers/ipn_controller.rb
@@ -1,6 +1,11 @@
 class IpnController < ApplicationController
   def index
     response = Hash.from_xml(request.body.read)
+
+    puts response
+
+    render :json => response
+=begin
     r = response["InstantPaymentNofication"]
     msisdn = r["MSISDN"]
     business_short_code = r["BusinessShortCode"]
@@ -17,6 +22,7 @@ class IpnController < ApplicationController
       render :json => {'status': 'ok'}
       check_order(invoice_number, trans_amount, trans_id)
     end
+=end
   end
 
 
