ebc858dabc49dcb49d6b96249fc74633a93d08b8
Martin Ndeto <martindeto@gmail.com>
2017-11-09 05:35:25 +0300

    confrimation email

--- app/controllers/user_confirmations_controller.rb
+++ app/controllers/user_confirmations_controller.rb
@@ -1,6 +1,19 @@
 class UserConfirmationsController < Devise::ConfirmationsController
   def new
     self.resource = resource_class.new
-    set_login
+    store_login
+  end
+
+  # POST /resource/confirmation
+  def create
+    self.resource = resource_class.send_confirmation_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
+    else
+      flash[:alert] = "This email is already confirmed!"
+      redirect_to(request.referer)
+    end
   end
 end
