84bf814dae31d8ebc9b5d804db2dd108edf4c065
Martin Ndeto <martindeto@gmail.com>
2017-11-14 15:23:52 +0300

    FOrce verification

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -572,7 +572,7 @@ end
   end
 
   def delivery_params
-    params.require(:store).permit(:collection_point, :collection_point_status, :lng, :lat, :detailed_location, :auto_delivery_status, :sendy_username, :sendy_key, :manual_delivery_status, :manual_delivery_instructions, :auto_delivery_location)
+    params.require(:store).permit(:collection_point, :collection_point_status, :detailed_location, :manual_delivery_status, :manual_delivery_instructions, :auto_delivery_location)
   end
 
   def pages_params
