54af737a228fa5297cbe066747279ec53334df77
Martin Ndeto <martindeto@gmail.com>
2018-05-31 16:33:27 +0300

    Admin UPdates

--- public/assets/application-b9049dde3a72b62fcd9591b9d75345459b1782004f1af81573e260e325fc624d.js
+++ public/assets/application-b9049dde3a72b62fcd9591b9d75345459b1782004f1af81573e260e325fc624d.js
@@ -29783,7 +29783,8 @@ $(function () {
         passw = $("#sendykey").val();
         lat = $("#store_lat").val();
         lng = $("#store_lng").val();
-        //alert(loc + user + passw);
+
+        $(".status").removeAttr('hidden').html('<i class="icon-spinner icon-spin"></i> Please wait as we verify your credentials');
 
         $.ajax({
             url: '/stores/sendy',
@@ -29803,6 +29804,10 @@ $(function () {
 
     $('#myOrderTable').DataTable({
         "order": [[1, "desc"]]
+    });
+
+    $('#myTable').DataTable({
+        "order": [[2, "desc"]]
     });
 
     $('#myHistoryTable').DataTable({
@@ -29915,8 +29920,8 @@ $(function () {
         $.ajax({
             url: '/carts/confirm',
             method: 'post',
-            data:{
-                ref:ref
+            data: {
+                ref: ref
             },
             success: function (e) {
                 if (e == "complete") {
@@ -30235,14 +30240,14 @@ $(function () {
         code = $("#delivery_code").val();
         ref = $("#order_ref").val();
         $.ajax({
-            url:'/stores/close_order',
-            method:'post',
-            data:{
-                code:code,
-                ref:ref
+            url: '/stores/close_order',
+            method: 'post',
+            data: {
+                code: code,
+                ref: ref
             },
-            success:function (e) {
-               // alert(e);
+            success: function (e) {
+                // alert(e);
             }
         })
     });
@@ -30250,16 +30255,59 @@ $(function () {
     $("#change_shipping_status").click(function () {
         ref = $("#order_ref").val();
         $.ajax({
-            url:'/stores/update_order2',
-            method:'post',
-            data:{
-                status:3,
-                ref:ref
+            url: '/stores/update_order2',
+            method: 'post',
+            data: {
+                status: 3,
+                ref: ref
             },
-            success:function (e) {
+            success: function (e) {
                 // alert(e);
             }
         })
+    });
+
+    $("#changepass").change(function (e) {
+        password = $("#changepass").val();
+        if (!pass(password)) {
+            $(".store_password_prev").html("<p style='color:red;font-size: 15px;'>Password must be at least 6 characters long, with at least one capital letter and number</p>");
+            $("#changepass").attr('style', 'text-align:center;border-bottom-color: red;box-shadow: 0 2px 2px -2px #FF0000;');
+        } else {
+            $(".store_password_prev").html("");
+            $("#changepass").attr('style', 'text-align:center;border-bottom-color: green;box-shadow: 0 2px 2px -2px #008000;');
+
+        }
+        store_change_reg();
+    });
+
+    $("#changepassconf").keyup(function (e) {
+        password = $("#changepass").val();
+        passwordc = $("#changepassconf").val();
+        if (password != passwordc) {
+            $(".store_password_conf_prev").html("<p style='color:red;font-size: 15px;'>Passwords don't match</p>");
+            $("#changepassconf").attr('style', 'text-align:center;border-bottom-color: red;box-shadow: 0 2px 2px -2px #FF0000;');
+        } else {
+            $(".store_password_conf_prev").html("");
+            $("#changepassconf").attr('style', 'text-align:center;border-bottom-color: green;box-shadow: 0 2px 2px -2px #008000;');
+
+        }
+        store_change_reg();
+    });
+
+    $("#changemail").change(function () {
+        email = $("#changemail").val();
+        if (valmail(email)) {
+            $(".display_email_prev").html("");
+            $("#changemail").attr('style', 'text-align:center;border-bottom-color: green;box-shadow: 0 2px 2px -2px #008000;');
+        } else {
+            $(".display_email_prev").html("<p style='color:red;font-size: 15px;'>Please enter a valid email</p>");
+            $("#changemail").attr('style', 'text-align:center;border-bottom-color: red;box-shadow: 0 2px 2px -2px #FF0000;');
+        }
+        store_change_reg();
+    });
+
+    $("#currentpass").keyup(function () {
+        store_change_reg();
     });
 });
 
@@ -30285,7 +30333,7 @@ function geocodeAddress(geocoder, resultsMap) {
         if (status === 'OK') {
 
             var string = "<br> <p>Click on a location below</p> <br>";
-           //console.log(results);
+            //console.log(results);
             results.forEach(function (entry) {
                 string = string + '<p style="border-color:black;padding: 1px;border-style: solid" onclick=\'selectlocation("' + String(entry['formatted_address']) + '");\'>' + String(entry['formatted_address']) + '</p>';
 
@@ -30391,7 +30439,7 @@ function val_ship() {
     phone = $("#ship_phone_number").val();
     email = $("#ship_email").val();
 
-    if (full_name != "" && phonenumbers(phone) && validateEmail(email)) {
+    if (full_name != "" && phonenumbers(phone) && valmail(email)) {
         if ($("#auto").is(':checked')) {
             $(".warn_fill_fields").html("<p style='color: green'>Type below to search for your nearest location</p>");
         } else {
@@ -30749,6 +30797,19 @@ function store_reg() {
     } else {
         $("#store_sign_up").attr("disabled", "true");
 
+    }
+
+}
+
+function store_change_reg() {
+    password = $("#changepass").val();
+    passwordc = $("#changepassconf").val();
+    email = $("#changemail").val();
+    curr = $("#currentpass").val();
+    if ((password == passwordc && pass(password) && valmail(email)) || (password == "" && passwordc == "" && valmail(email) && curr != "")) {
+        $("#changebtn").removeAttr("disabled");
+    } else {
+        $("#changebtn").attr("disabled", "disabled");
     }
 
 }
