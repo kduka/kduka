5ba2cddebc1a5edeeedc0bf5862758acc1d2bb35
Martin Ndeto <martindeto@gmail.com>
2018-06-23 15:27:21 +0300

    Test store id capture

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -3,4 +3,17 @@ class Ahoy::Visit < ApplicationRecord
 
   has_many :events, class_name: "Ahoy::Event"
   belongs_to :user, optional: true
+
+  before_save :finalize
+
+
+  def finalize
+    @store = Store.where(c_subdomain:request.subdomain,domain:request.domain,own_domain:true).first
+
+    if @store.nil?
+      @subdomain = request.subdomain[/(\w+)/]
+      @store = Store.where(subdomain: @subdomain).first
+    end
+  self[:utm_campaign] = @store.id rescue nil
+  end
 end
