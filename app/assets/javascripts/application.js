59c8cdf54f1728b6f525f252615504577239d0b2
Martin Ndeto <martindeto@gmail.com>
2018-08-07 18:43:15 +0300

    Test whether modal is working online

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,7 +32,7 @@ $(function () {
         e.preventDefault();
     });
 
-    $(".signup").click(function (e) {
+    $("#sign").click(function (e) {
         $.ajax({
             url: '/stores/signup',
             method: 'get',
