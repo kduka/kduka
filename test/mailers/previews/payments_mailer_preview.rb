ac5ac8a14d95f4b3d84254e27d24069a2a6598e1
Martin Ndeto <martindeto@gmail.com>
2018-09-03 17:04:39 +0300

    Varaints complete, showing on email too

--- test/mailers/previews/payments_mailer_preview.rb
+++ test/mailers/previews/payments_mailer_preview.rb
@@ -1,7 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/payments_mailer
 class PaymentsMailerPreview < ActionMailer::Preview
   def full_payment_recieved()
-    PaymentsMailer.full_payment_recieved(Order.find(17))
+    PaymentsMailer.full_payment_recieved(Order.find(19))
   end
 
   def merchant_payment_recieved()
