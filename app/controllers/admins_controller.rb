60cb6990fd79786939d887b692e54ab1e623b7c7
Martin Ndeto <martindeto@gmail.com>
2018-12-30 08:24:26 +0300

    SHow only complete subscriptions

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -170,7 +170,7 @@ class AdminsController < ApplicationController
   end
 
   def subscriptions
-    @subscriptions = Subscription.all.order( 'created_at DESC' )
+    @subscriptions = Subscription.where(:order_status_id => 6)
     super_admin
   end
 
