c67713f7555e57b48ba6c754929743cecc566f75
Martin Ndeto <martindeto@gmail.com>
2019-03-04 12:05:08 +0300

    Bad seed

--- db/migrate/20190225100839_add_index_to_shop_category.rb
+++ db/migrate/20190225100839_add_index_to_shop_category.rb
@@ -1,3 +1,3 @@
 class AddIndexToShopCategory < ActiveRecord::Migration[5.0]
-  add_index :shop_categories, :shop_category, uniqueness: {case_sensitive: false}
+  add_index :shop_categories, :shop_category, unique: {case_sensitive: false}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -193,6 +193,25 @@ ActiveRecord::Schema.define(version: 20190228113442) do
     t.index ["store_id"], name: "index_feedbacks_on_store_id", using: :btree
   end
 
+  create_table "fin_ipns", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.string   "mobileNumber"
+    t.string   "customer_reference"
+    t.string   "date"
+    t.string   "transaction_reference"
+    t.string   "paymentMode"
+    t.string   "amount"
+    t.string   "till"
+    t.string   "billNumber"
+    t.string   "servedBy"
+    t.string   "additionalInfo"
+    t.string   "bank_reference"
+    t.string   "transactionType"
+    t.string   "account"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
+
   create_table "fonts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -354,9 +373,7 @@ ActiveRecord::Schema.define(version: 20190228113442) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "explore_image"
-    t.string   "string"
     t.index ["shop_category"], name: "index_shop_categories_on_shop_category", unique: true, using: :btree
-    t.index ["shop_category"], name: "shop_category", unique: true, using: :btree
   end
 
   create_table "store_amounts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
