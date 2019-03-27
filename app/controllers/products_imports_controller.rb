4af8fed030135c0fcbd70b4a9ae9cad37fcac89b
kamundia <kimani@kduka.co.ke>
2019-03-27 14:26:59 +0300

    right excel-upload

---
+++ app/controllers/products_imports_controller.rb
@@ -0,0 +1,31 @@
+class ProductsImportsController < ApplicationController
+  def new
+  @products_import = ProductsImport.new
+  set_shop_show
+  end
+
+def create
+  @products_import = ProductsImport.new(params[:products_import])
+   if @products_import.save
+    flash[:notice] = "Products imported successfully!"
+    redirect_to products_manage_path
+  else
+    flash[:alert] = "Products not imported.Kindly confirm the spreadsheet then try again."
+
+    redirect_to products_imports_new_path(@products_import, params:request.query_parameters)
+  end
+
+end
+def export
+  @products = Product.where(store_id: current_store.id)
+  @categories = Category.where(store_id: current_store.id)
+  respond_to do |format|
+   format.xlsx{
+     response.headers[
+       'Content-Disposition'
+     ] = "attachment; filename= #{current_store.name}.xlsx"
+   }
+
+ end
+end
+end
---
+++ app/models/products_import.rb
@@ -0,0 +1,54 @@
+class ProductsImport
+  include ActiveModel::Model
+  require 'roo'
+
+  attr_accessor :file
+
+  def initialize(attributes={})
+    attributes.each { |name, value| send("#{name}=", value) }
+  end
+
+  def persisted?
+    false
+  end
+
+  def open_spreadsheet
+    case File.extname(file.original_filename)
+    when ".csv" then Csv.new(file.path, nil, :ignore)
+    when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
+    when ".xlsx" then Roo::Excelx.new(file.path)
+    else raise "Unknown file type: #{file.original_filename}"
+    end
+  end
+
+  def load_imported_products
+    spreadsheet = open_spreadsheet
+    header = spreadsheet.row(6)
+    (7..spreadsheet.last_row).map do |i|
+      row = Hash[[header, spreadsheet.row(i)].transpose]
+      row[:store_id] = $store
+      product = Product.find_by_id(row["id"]) || Product.new
+      product.attributes = row.to_hash
+      product
+    end
+  end
+
+  def imported_products
+    @imported_products ||= load_imported_products
+  end
+
+  def save
+    if imported_products.map(&:valid?).all?
+      imported_products.each(&:save!)
+      true
+    else
+      imported_products.each_with_index do |product, index|
+        product.errors.full_messages.each do |msg|
+          errors.add :base, "Row #{index + 7}: #{msg}"
+        end
+      end
+      false
+    end
+  end
+
+end
