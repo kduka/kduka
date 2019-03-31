09acf34d832155e2fbcd3323616d7152a5c5b051
Justice Towett <jtowett89@gmail.com>
2019-03-31 12:06:50 +0300

    Mailers

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -4,7 +4,7 @@ class Ahoy::Visit < ApplicationRecord
   has_many :events, class_name: "Ahoy::Event"
   belongs_to :user, optional: true
 
-  # before_save :finalize
+  before_save :finalize
 
 
   def finalize
