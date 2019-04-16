27b809509583f7e2c0211c094ee64e6c9755ae7a
Martin Ndeto <martindeto@gmail.com>
2019-04-16 12:41:16 +0300

    Delete test

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,7 @@
 class Subscription < ApplicationRecord
   belongs_to :store
   belongs_to :order_status
+
+  has_many :invoice, :dependent => :destroy
+  has_many :subscription_record, :dependent => :destroy
 end
