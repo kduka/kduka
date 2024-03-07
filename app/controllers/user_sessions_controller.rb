b7621de25fb6475c6d1c24da7c6f91e86a32ab31
Annastacia Mumbua Kioko <anne@mradi.co.ke>
2024-03-07 10:10:10 +0300

    fix: syntax error in user sessions controller and logo size in users sign in

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,7 +2,9 @@ class UserSessionsController < Devise::SessionsController
   prepend_before_action :require_no_authentication, only: [:new, :create]
   prepend_before_action :allow_params_authentication!, only: :create
   prepend_before_action :verify_signed_out_user, only: :destroy
-  prepend_before_action only: [:create, :destroy] { request.env["devise.skip_timeout"] = true }
+  prepend_before_action only: [:create, :destroy] do
+    request.env["devise.skip_timeout"] = true
+  end
 
   # GET /resource/sign_in
   def new
